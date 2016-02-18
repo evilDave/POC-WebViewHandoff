26127ff4f58b8b6b98d110f87c1f3b8836525121
David Clark <david.clark@hotelscombined.com>
2016-02-18 13:16:46 +1100

    load real html with embedded images animate alpha with doublue buffering of two webviews - might re-use this technique on the provider pages... maybe something like it

--- WebViewHandoff/WebViewController.m
+++ WebViewHandoff/WebViewController.m
@@ -13,6 +13,7 @@
 // TODO: I will redo the views separately with a base class for handling the basic functionality of the webview, this is to fix up the mess that is the view stack messaging, the load/reload/init/reinit rubbish
 
 @implementation WebViewController {
+	WKWebView *_loadingWebView;
 	WKWebView *_webView;
 }
 
@@ -24,16 +25,25 @@
 	[_webView setTranslatesAutoresizingMaskIntoConstraints:NO];
 	[_webView setNavigationDelegate:self];
 	[self.view addSubview:_webView];
-	[[_webView leadingAnchor] constraintEqualToAnchor:[self.view leadingAnchor]].active = YES;
-	[[_webView trailingAnchor] constraintEqualToAnchor:[self.view trailingAnchor]].active = YES;
-	[[_webView topAnchor] constraintEqualToAnchor:[self.view topAnchor]].active = YES;
-	[[_webView bottomAnchor] constraintEqualToAnchor:[self.view bottomAnchor]].active = YES;
+	[[_webView topAnchor] constraintEqualToAnchor:self.view.topAnchor].active = YES; // the website is doing something funky to mode the site down under the bar, it should probably not
+	[[_webView bottomAnchor] constraintEqualToAnchor:self.bottomLayoutGuide.topAnchor].active = YES;
+	[[_webView leadingAnchor] constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+	[[_webView trailingAnchor] constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
 
-	[_webView loadLoadingPage];
+	_loadingWebView = [[WKWebView alloc] init];
+	[_loadingWebView setTranslatesAutoresizingMaskIntoConstraints:NO];
+	[self.view addSubview:_loadingWebView];
+	[[_loadingWebView topAnchor] constraintEqualToAnchor:self.topLayoutGuide.bottomAnchor].active = YES;
+	[[_loadingWebView bottomAnchor] constraintEqualToAnchor:self.bottomLayoutGuide.topAnchor].active = YES;
+	[[_loadingWebView leadingAnchor] constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+	[[_loadingWebView trailingAnchor] constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+	[_loadingWebView loadLoadingPage];
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
-	[_webView loadLoadingPage];
+	// this stuff is not valid for disappearing going to the provider controller, okay for demo
+	[_webView loadBlankPage];
+	[_loadingWebView setAlpha:1];
 }
 
 - (void)load:(NSString *)uri {
@@ -44,7 +54,6 @@
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
 	if([[navigationAction request] isHomePage]) {
-		[_webView loadBlankPage];
 		[self.navigationController popViewControllerAnimated:YES];
 		decisionHandler(WKNavigationActionPolicyCancel);
 	}
@@ -53,22 +62,27 @@
 	}
 }
 
-//- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
-//}
-//
-//- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
-//}
-//
-//- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
-//}
-//
-//- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
-//}
-//
-//- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
-//}
-//
-//- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView {
-//}
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+	if(![webView.URL.scheme isEqualToString:@"about"]) {
+		[UIView animateWithDuration:0.2 animations:^{
+			[_loadingWebView setAlpha:0];
+		}];
+	}
+}
+
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+}
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+}
+
+- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView {
+}
 
 @end
